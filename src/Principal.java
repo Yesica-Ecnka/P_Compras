15c9767984ac929c73695837373e7a2734d66f30
Yesica-Ecnka <ypart@miumg.edu.gt>
2024-07-07 13:49:51 -0600

    Funciono menos por el while

---
+++ src/Principal.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Scanner telclado = new Scanner(System.in);
+        System.out.println("Ingrese el límite de la tarjeta, por favor: ");
+        double limite = telclado.nextDouble();
+        TarjetaDeCredito tarjeta = new TarjetaDeCredito(limite);
+
+
+        int salir = 1;
+        while(salir != 0){
+            System.out.println("Escriba la descripción de la compra: ");
+            String descripcion = telclado.next();
+
+            System.out.println("Escriba el valor de la comprar: ");
+            double valor = Double.valueOf(telclado.next());
+
+            Compra compra = new Compra(valor, descripcion);
+            boolean compraRealizada = tarjeta.lanzarCompra(compra);
+
+            if(compraRealizada){
+                System.out.println("Compra relizada!");
+                System.out.println("Escriba 0 para salir o 1 para continuar: ");
+                salir = telclado.nextInt();
+            }else{
+                System.out.println("Saldo insuficiente :/");
+                salir = 0;
+            }
+
+            System.out.println("**********************************************");
+            System.out.println("Compras realizadas!: \n" );
+            for (Compra compra1 : tarjeta.getListaDeCompras()){
+                System.out.println(compra.getDescripcion() + "--" + compra.getValor() );
+            }
+
+            System.out.println("***********************************************");
+            System.out.println("\n Saldo de la tarjeta: " + tarjeta.getSaldo());
+        }
+    }
+}
--- src/TarjetaDeCredito.java
+++ src/TarjetaDeCredito.java
@@ -1,3 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TarjetaDeCredito {
-    private int saldo;
+    private double limite;
+    private double saldo;
+    private List<Compra> listaDeCompras; //un atributo
+
+    public TarjetaDeCredito(double limite) {
+        this.limite = limite;
+        this.saldo = limite;
+        this.listaDeCompras = new ArrayList<>(); //acá llame a mi atributo y lo instancie a una ArrayLidt
+    } //se creó un constructor.
+
+    public boolean lanzarCompra(Compra compra){
+        if(this.saldo >= compra.getValor()){
+            this.saldo -= compra.getValor();
+            this.listaDeCompras.add(compra);
+            return true;
+        }
+        return false;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public List<Compra> getListaDeCompras() {
+        return listaDeCompras;
+    }
 }
